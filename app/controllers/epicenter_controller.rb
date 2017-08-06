2ad40f952f99b850d33c96bfe09a4a041a04b80f
Bart De Nef <bartdn183@gmail.com>
2017-08-06 14:47:58 -0400

    update

--- app/controllers/epicenter_controller.rb
+++ app/controllers/epicenter_controller.rb
@@ -1,4 +1,10 @@
 class EpicenterController < ApplicationController
   def index
   end
+
+  def my_location
+  	if params[:my_location]
+  		@my_location = UserLocation.create(my_location: params[:my_location])
+  	end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root 'epicenter#index'
 
   devise_for :users
+
+  post 'my_location' => 'epicenter#my_location'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
